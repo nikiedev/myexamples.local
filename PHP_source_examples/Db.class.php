a63fd127d7ae9ccaf5e8b4e715e07c24a934f098
Igor <igor.nikiforov@ukr.net>
2016-07-10 15:44:49 +0300

    Add files via upload
    db file

---
+++ PHP_source_examples/Db.class.php
@@ -0,0 +1,55 @@
+<?php
+
+
+namespace app\lib;
+
+/**
+ * Class Db | Класс для работы с БД
+ * @package app\lib
+ */
+class Db
+{
+    private $dbh;
+
+    /**
+     * Db constructor.
+     * Соединение с Базой Данных (настройки -> config.php)
+     */
+    public function __construct()
+    {
+        require_once dirname(dirname(__DIR__)) . '/config.php';
+        $dsn = 'mysql:host=' . DB_HOST . ';dbname=' . DB_NAME . ';charset=utf8';
+        $this->dbh = new \PDO(
+            $dsn, DB_USER, DB_PASS, [
+                \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
+                \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC
+            ]
+        );
+    }
+
+    /**
+     * @param string $sql
+     * @return bool
+     */
+    public function execute(string $sql): bool 
+    {
+        $sth = $this->dbh->prepare($sql);
+        return $sth->execute();
+    }
+
+    /**
+     * @param string $sql
+     * @param array $params
+     * @return array
+     * @throws \Exception
+     */
+    public function query(string $sql, array $params = []): array
+    {
+        $sth = $this->dbh->prepare($sql);
+        $res = $sth->execute($params);
+        if (false !== $res) {
+            return $sth->fetchAll();
+        }
+        return [];
+    }
+}
