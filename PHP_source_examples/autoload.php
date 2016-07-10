9931d3a01a6ff09712e717f0d5490dfaf288c346
Igor <igor.nikiforov@ukr.net>
2016-07-10 15:41:53 +0300

    Update autoload.php

--- PHP_source_examples/autoload.php
+++ PHP_source_examples/autoload.php
@@ -4,8 +4,14 @@
  * @param $class_name
  * Example: App\Models\User => ./App/Models/User.php
  */
-
 function __autoload($class_name)
 {
     require __DIR__ . '/' . str_replace('\\', '/', $class_name) . '.php';
 }
+
+/**
+ --- 2 вариант ---
+
+spl_autoload_extensions('.class.php');
+spl_autoload_register();
+*/
