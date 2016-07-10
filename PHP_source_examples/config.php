9520a54d39cdd9659435009bf0636ec94e7d8fe8
Igor <igor.nikiforov@ukr.net>
2016-07-10 15:49:18 +0300

    db pdo configuration file

---
+++ PHP_source_examples/config.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Database connection settings
+ * Настройки подключения к Базе Данных
+ * --> Db.class.php
+ */
+const DB_HOST = 'localhost';
+const DB_NAME = 'kruiz';
+const DB_USER = 'root';
+const DB_PASS = '';
